22a0328e130df9fe405c0d3ae139b2809474a7b8
Phillip Irwin <irwin.phillip@gmail.coom>
2026-07-23 01:06:43 -0500

    Just some more prep

--- pomo-go.go
+++ pomo-go.go
@@ -5,11 +5,24 @@ import (
 	"time"
 )
 
+type Timer struct {
+	work string
+	rest string
+}
+
+var (
+	short    = Timer{"15m", "3m"}
+	standard = Timer{"25m", "5m"}
+	long     = Timer{"50m", "10m"}
+)
+
 func statusUpdate() string { return "" }
 
 func main() {
+	fmt.Printf("%v\n%v\n%v\n", short, standard, long)
 	c := time.Tick(5 * time.Second)
 	for next := range c {
 		fmt.Printf("%v %s\n", next, statusUpdate())
 	}
+
 }
